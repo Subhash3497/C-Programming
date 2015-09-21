2762df5d54a54613595cd0631957e978374d3ded
Subhash3497 <subhash3497@gmail.com>
2015-09-21 19:51:11 -0400

    2

--- Program 4/main.c
+++ Program 4/main.c
@@ -13,12 +13,29 @@ int main(int argc, const char * argv[])
     //Variables
     int days;
     int flights;
-    int distance;
+    float distance [5] = {};
     int average;
 
+
+
+
     //Assignment
-    for (<#initialization#>; <#condition#>; <#increment#>) {
-        <#statements#>
+    for (printf("How man days has your dragon been practicing?\n"),scanf("%d",&days); days != 0; days--)
+    {
+        for (printf("How many flights were completed on day %d\n",days), scanf("%d",&flights); flights != 0; flights--)
+        {
+            for (printf("How long was flight %d\n",flights), scanf("%f",&distance); ;) {
+                break;
+
+
+            }
+
+        }
+
+
+
+
+
     }
 
 
