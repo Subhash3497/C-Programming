ea0ae079103db90c063e72c61a70e361f8ff16f8
Subhash3497 <subhash3497@gmail.com>
2015-09-24 20:47:03 -0400

    Final

--- Program 4/main.c
+++ Program 4/main.c
@@ -1,21 +1,19 @@
-//
-//  main.c
-//  Program 4
-//
-//  Created by Sebastian on 9/21/15.
-//  Copyright © 2015 Subhash. All rights reserved.
-//
+//Subhash Naidu
+//COP3223C-15 0R04
+//Program 4
+//9/24/2015
+//prints the avg distance of a dragons practice flights.
+
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[])
+int main()
 {
     //Variables
-    int days, days_input;
-    int flights, flights_input;
-    float distance, distance_input;
-    int average;
-
+    int days, days_input = 0;
+    int flights, flights_input = 0;
+    float distance = 0.0, distance_input = 0.0;
+    float average;
 
     //Assignment
     printf("How many days has your dragon been practicing?\n");
@@ -23,27 +21,21 @@ int main(int argc, const char * argv[])
 
     for (days = 1; days <= days_input; days++)
     {
-        printf("How many flights were completed in day %d\n",days);
+        printf("How many flights were completed on day %d\n",days);
         scanf("%d",&flights_input);
 
         for (flights = 1; flights <= flights_input; flights++)
         {
             printf("How long was flight %d\n",flights);
-            scanf("%d",&distance);
+            scanf("%f",&distance_input);
+            distance = distance + distance_input;
         }
 
+        average = distance / flights_input;
+        printf("The average distance for this day is %.3f.\n\n", average);
+        distance = 0;
+
     }
-
-
-
-
-
-
-
-
-
-
-
 
     return 0;
 }
