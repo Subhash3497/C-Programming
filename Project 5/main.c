93c7b4ed3886919f59843e61568370298d005075
Subhash3497 <subhash3497@gmail.com>
2015-10-04 16:47:50 -0400

    Initial Commit

---
+++ Project 5/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Project 5
+//
+//  Created by Sebastian on 10/4/15.
+//  Copyright © 2015 Subhash. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
