338d2401a707a837880f142809fbf4db3d73951e
Subhash3497 <subhash3497@gmail.com>
2015-10-05 16:53:14 -0400

    1st

--- Project 5/main.c
+++ Project 5/main.c
@@ -1,15 +1,33 @@
 //
 //  main.c
 //  Project 5
-//
-//  Created by Sebastian on 10/4/15.
-//  Copyright © 2015 Subhash. All rights reserved.
-//
+//  Created by Subhash on 10/4/15.
+
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main() {
+    FILE * ifp;
+    char filename[30];
+    int templow;
+    int temphigh;
+    int month, day , year;
+    float temp;
+    float sum;
+
+
+    //User Prompt and Input
+    printf("Tell me about your dragons preffered temperature for flying:\n");
+    printf("What is the coldest temperature they can fly in ?\n");
+    scanf("%d",&templow);
+    printf("What is the hottest temperature they can fly in ?\n");
+    scanf("%d",&temphigh);
+    printf("Please enter the name of the weather data file for Dragon Island.\n");
+    scanf("%s",&filename);
+
+
+
+
+
     return 0;
 }
